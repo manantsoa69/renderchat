c75611e793a942b9513b77c4da54054b363be7ee
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2023-08-24 21:51:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// index.js
 const express = require('express');
-
-const compression = require('compression'); // Import the compression middleware
-
+const compression = require('compression');
 require('dotenv').config();
+
 const webApp = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,12 +11,25 @@ webApp.use(compression());
 webApp.use(express.urlencoded({ extended: true }));
 webApp.use(express.json());
 
+
+
 // Load other routes and middlewares
 const fbWebhookRoute = require('./routes/fbWebhookRoute');
 const homeRoute = require('./routes/homeRoute');
 
 webApp.use('/facebook', fbWebhookRoute.router);
 webApp.use('/', homeRoute.router);
+
+
+// New route to handle a simple request
+webApp.get('/hello', (req, res) => {
+  // Log something after receiving the request
+
+  
+  res.send('Hello, this is a simple response from the server!');
+  console.log(' /up');
+});
+
 
 webApp.listen(PORT, () => {
   console.log(`Server is up and running at ${PORT}`);
