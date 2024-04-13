9527e62551b0ee1bc2930af05e39e4227225d260
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2024-04-14 01:12:10 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
 const express = require('express');
-const compression = require('compression');
-require('dotenv').config();
+const path = require('path');
+const Redis = require('ioredis');
+const dotenv = require('dotenv');
+dotenv.config();
+const { getStoredNumbers } = require('./redis');
+
+const homeRoute = require('./routes/homeRoute');
 
 const webApp = express();
 const PORT = process.env.PORT || 3000;
 
-// Use the compression middleware to enable Gzip compression
-webApp.use(compression());
+// Redis Setup
+const redisUrl = process.env.NUB_SAVE || '';
+const redisClient = new Redis(redisUrl);
+redisClient.on('connect', () => console.log('Connected to upstash'));
+redisClient.on('error', error => console.error('Error connecting to Redis:', error));
 
+
+webApp.use(express.static(path.join(__dirname, 'public')));
 webApp.use(express.urlencoded({ extended: true }));
 webApp.use(express.json());
 
 
 
-// Load other routes and middlewares
-const fbWebhookRoute = require('./routes/fbWebhookRoute');
-const homeRoute = require('./routes/homeRoute');
-
-webApp.use('/facebook', fbWebhookRoute.router);
-webApp.use('/', homeRoute.router);
-
-
-// New route to handle a simple request
-webApp.get('/hello', (req, res) => {
-  // Log something after receiving the request
-
-  
-  res.send('Hserver!');
-  console.log(' /up');
+// Error Handling Middleware
+webApp.use((err, req, res, next) => {
+  console.error('An error occurred:', err);
+  res.status(500).json({ error: 'Internal Server Error' });
 });
 
+const fbWebhookRoute = require('./routes/fbWebhookRoute');
+webApp.use('/facebook', fbWebhookRoute.router);
+webApp.use('/subscribe', homeRoute.router);
 
+// Query Route
+webApp.get('/query', async (req, res) => {
+  try {
+    const numberToQuery = req.query.number || '';
+
+    // Assuming the getStoredNumbers() function returns an array of items from Redis
+    const items = await getStoredNumbers(numberToQuery);
+
+    // Delete the data from Redis after querying
+    await Promise.all(
+      items.map(async (item) => {
+        const key = item.number; // Assuming the "number" field is used as the Redis key
+        await redisClient.del(key);
+      })
+    );
+
+    res.json({ numberToQuery, items });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+// Start the Server
 webApp.listen(PORT, () => {
   console.log(`Server is up and running at ${PORT}`);
 });
